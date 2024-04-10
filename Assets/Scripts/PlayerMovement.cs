ca5a3fe16032c2fa08cccf1f50669d5504e33366
HienaDev <ant.rodrigues1999@gmail.com>
2024-04-10 14:00:03 +0100

    Change tongue to go in a straight direction and applied changes to prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -541,14 +541,16 @@ public class PlayerMovement : MonoBehaviour
         float gravity = Physics.gravity.y;
         float displacementY = endPoint.y - startPoint.y;
 
-        Vector3 displacementXZ = new Vector3(endPoint.x- startPoint.x, 0f, endPoint.z -startPoint.z);
+        Vector3 displacementXZ = new Vector3(endPoint.x - startPoint.x, 0f, endPoint.z - startPoint.z);
 
         Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
 
         Vector3 velocityXZ = displacementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity)
             + Mathf.Sqrt(2 * (displacementY - trajectoryHeight) / gravity));
 
-        return (velocityXZ + velocityY);
+        //return (velocityXZ + velocityY);
+
+        return (endPoint - startPoint) * 4;
     }
 
     private void UpdateMotion()
