a51fe9424b2777a6c64e887b278669bf89266588
Rafael José <ra.david@hotmail.com>
2024-05-11 17:27:40 +0100

    Update debugger

--- Assets/Scripts/Debug/Debugger.cs
+++ Assets/Scripts/Debug/Debugger.cs
@@ -15,32 +15,44 @@ public class Debugger : MonoBehaviour
 
     void Update()
     {
+        // Full health player
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha1))
         {
             playerHealth.FullHealth();
         }
 
+        // Regen player
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha2))
         {
             playerHealth.Regen(10);
         }
 
+        // Damage player
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha3))
         {
             playerHealth.Damage(10);
         }
 
+        // Increase player speed
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha4))
         {
             playerMovement.SetVelocity(50);
         }
 
+        // Decrease player speed
         if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            playerMovement.SetVelocity(10);
+        }
+
+        // Set damage
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha6))
         {
             dealDamage.SetDamage(100);
         }
-
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha6))
+        
+        // Stop enemies from looking for player
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha7))
         {
             foreach (EnemyController enemy in FindObjectsOfType<EnemyController>())
             {
@@ -48,7 +60,8 @@ public class Debugger : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha7))
+        // Make enemies look for player
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetKeyDown(KeyCode.Alpha8))
         {
             foreach (EnemyController enemy in FindObjectsOfType<EnemyController>())
             {
