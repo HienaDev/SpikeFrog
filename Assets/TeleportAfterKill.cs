f616eb70d5e147033f18a356eaba7e15371eaa0c
HienaDev <ant.rodrigues1999@gmail.com>
2024-05-27 16:49:18 +0100

    Add teleport to lab after killing all enemies

---
+++ Assets/TeleportAfterKill.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportAfterKill : MonoBehaviour
+{
+    [SerializeField] private GameObject[] enemiesToKill;
+    [SerializeField] private Transform teleportTo;
+    [SerializeField] private GameObject player;
+
+    private void FixedUpdate()
+    {
+
+        // Check if all objects in the array have been destroyed
+        bool allDestroyed = true;
+        foreach (GameObject obj in enemiesToKill)
+        {
+            if (obj.activeSelf)
+            {
+                allDestroyed = false;
+                break;
+            }
+        }
+
+        // If all objects are destroyed, perform the action
+        if (allDestroyed)
+        {
+            player.transform.position = teleportTo.position;
+            Destroy(gameObject, 0.1f);
+        }
+    }
+}
