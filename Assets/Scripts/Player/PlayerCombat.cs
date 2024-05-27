79830edd47bd1944da753b7efe4d51fe51e38e6f
HienaDev <ant.rodrigues1999@gmail.com>
2024-05-27 09:51:17 +0100

    Update player animation speed

--- Assets/Scripts/Player/PlayerCombat.cs
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -34,7 +34,7 @@ public class PlayerCombat : MonoBehaviour
             // player is unfrozen through idle animation using method DisableFreeze from this method
             playerMovement.EnableFreeze();
         }
-        else if (Input.GetMouseButtonDown(0) && !puncheable && timerCombo > timeAvaiableForNextCombo)
+        else if (Input.GetMouseButtonDown(0) && !puncheable)// && timerCombo > timeAvaiableForNextCombo)
         {
             animator.SetTrigger("Punch");
             timerCombo = 0;
