289fe5767b6b35e3d3c1a8b2a4f28eea8c8f7ead
Rafael José <ra.david@hotmail.com>
2024-06-10 16:04:19 +0100

    Refactor and remove unused sound files

--- Assets/Scripts/Scenario/DestroyAfterKill.cs
+++ Assets/Scripts/Scenario/DestroyAfterKill.cs
@@ -4,9 +4,10 @@ public class DestroyAfterKill : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemiesToKill;
 
+    private bool alreadyDestroyed = false;
+
     private void FixedUpdate()
     {
-
         // Check if all objects in the array have been destroyed
         bool allDestroyed = true;
         foreach (GameObject obj in enemiesToKill)
@@ -19,8 +20,9 @@ public class DestroyAfterKill : MonoBehaviour
         }
 
         // If all objects are destroyed, perform the action
-        if (allDestroyed)
+        if (allDestroyed && !alreadyDestroyed)
         {
+            alreadyDestroyed = true;
             gameObject.SetActive(false);
         }
     }
